b323730670f58e5cc5190a61cc0b0ff4092dc9f0
A-VOID-A-VOID-A-VOID <34438014+A-VOID-A-VOID-A-VOID@users.noreply.github.com>
2017-12-11 20:32:53 +1100

    Created gameplay scene

---
+++ PROTOTYPE_1/Assets/Scripts/MoveInMovementArea.cs
@@ -0,0 +1,23 @@
+﻿//////////////////////////////////////////////////
+// Author:              A_VOID
+// Date created:        11/12/17
+// Date last edited:    11/12/17
+//////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInMovementArea : MonoBehaviour
+{
+	// Called when the script is initialised.
+	private void Start()
+    {
+		
+	}
+	
+	// Called every time the gameplay logic is updated.
+	private void Update()
+    {
+		
+	}
+}
